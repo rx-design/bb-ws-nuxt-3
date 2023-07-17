027f54305864b9c6fb9f90ae52558666da356b1b
pjotr.surkov <pjotr.surkov@bigbank.ee>
2023-07-17 11:32:10 +0300

    Split code and create a component

--- app.vue
+++ app.vue
@@ -1,45 +1,7 @@
 <template>
   <div class="columns is-centered">
     <div class="column is-one-third">
-      <div class="card">
-        <header class="card-header">
-          <p class="card-header-title product-card">
-            {{ state.title }}
-          </p>
-        </header>
-        <div class="card-image">
-          <figure class="image is-4by3">
-            <img
-              src="https://picsum.photos/1280/960"
-              alt="Placeholder image"
-            >
-          </figure>
-        </div>
-        <div class="card-content">
-          <div class="media">
-            <div class="media-content">
-              <p class="title is-4">
-                {{ state.productName }}
-              </p>
-              <p class="subtitle is-6">
-                {{ state.productPrice }}
-              </p>
-            </div>
-          </div>
-          <div class="content">
-            {{ state.arrivalLabel }}
-            <br>
-            <time datetime="2023-9-1">
-              {{ state.arrivalDate }}
-            </time>
-          </div>
-        </div>
-        <footer class="card-footer">
-          <a href="#" class="card-footer-item">
-            {{ state.submitButtonLabel }}
-          </a>
-        </footer>
-      </div>
+      <ProductCard v-bind="state"/>
     </div>
   </div>
 </template>
@@ -54,11 +16,3 @@
     title: 'Trending product',
   })
 </script>
-
-<style>
-  @import "https://cdn.jsdelivr.net/npm/bulma@0.9.4/css/bulma.min.css";
-
-  .product-card {
-    color: orangered;
-  }
-</style>
